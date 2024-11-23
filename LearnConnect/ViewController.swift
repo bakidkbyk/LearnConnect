308be16f65b15b6b5029e9a0ab62cf7cecf42750
bakidkbyk <baki.ordulu@gmail.com>
2024-11-23 20:36:31 +0300

    first commit

---
+++ LearnConnect/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnConnect
+//
+//  Created by Baki Dikbıyık on 23.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
